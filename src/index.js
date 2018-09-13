f0d8de6ba2677d7348d72ffd69d1f2eb0481725b
Hannah Lazarus <hanhanhan.lazarus@gmail.com>
2018-09-12 20:13:36 -0700

    add prettier/eslint configs

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
-import {doInclude} from './other'
-var element = document.createElement('h1');
+import { doInclude } from './other'
+var element = document.createElement('h1')
 element.innerHTML = doInclude
+
+let x
+document.body.appendChild(element)
+document.body.appendChild(element)
+document.body.appendChild(element)
 
 document.body.appendChild(element)
